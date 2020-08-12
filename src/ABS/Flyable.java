236a4fa4fc18fdca91a1838078b3460eca9ff0fa
Vasiliy Kolisnichenko <yadrspin@gmail.com>
2020-08-12 14:31:24 +0900

    delete some folders
    delete some folders

--- src/ABS/Flyable.java
+++
@@ -1,5 +0,0 @@
-package ABS;
-
-public interface Flyable{
-    public void fly();
-}
--- src/Dog.java
+++
@@ -1,6 +0,0 @@
-public class Dog extends Animal {
-    @Override
-    public void Voice(){
-        System.out.println("Гав!");
-    }
-}
