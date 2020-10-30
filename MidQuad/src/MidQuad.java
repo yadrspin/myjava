86e7048fb0fe7e72be6373da07fc48bacecb58bf
Vasiliy Kolisnichenko <yadrspin@gmail.com>
2020-10-30 19:03:53 +0300

    Update MidQuad.java

--- MidQuad/src/MidQuad.java
+++ MidQuad/src/MidQuad.java
@@ -5,19 +5,31 @@ import java.util.ArrayList;
 
 public class MidQuad {
     public static void main(String[] args) throws IOException {
-        ArrayList<Integer> intList = new ArrayList<>();
+        ArrayList<Float> floatList = new ArrayList<>();
         try(BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in))) {
             System.out.println("Введите 10 чисел через пробел, после ввода нажмите Enter:)");
             for(int i = 0; i < 10; i++){
-                intList.add(Integer.parseInt(consoleReader.readLine()));
+                floatList.add(Float.parseFloat(consoleReader.readLine()));
             }
         }
     }
 
-    public int MidQuadSearch(ArrayList<Integer> list){
-        int result = 0;
-
-
+    public float MidQuadSearch(ArrayList<Float> floatList){
+        float result = 0;
+        float midArith = 0;
+        float dispers = 0;
+        ArrayList<Float> dispList = new ArrayList<>();
+        for (int i = 0; i < floatList.size(); i++) {
+            midArith = midArith + floatList.get(i);
+        }
+        midArith = midArith / 10;
+        for (int i = 0; i < floatList.size(); i++) {
+            dispList.add(floatList.get(i) * 2);
+        }
+        for (int i = 0; i < dispList.size(); i++) {
+            dispers = dispers + dispList.get(i);
+        }
+        result = dispers;
         return result;
     }
 }
